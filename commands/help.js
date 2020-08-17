29f731cda89a14ebb4f7c35b49012830801fd292
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-08-17 14:32:53 +0530

    Allow help command to have arguments
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

--- commands/help.js
+++ commands/help.js
@@ -1,25 +1,62 @@
 exports.run = (client, message, args) => {
   const { Client, MessageEmbed } = require('discord.js');
-  const embed = new MessageEmbed()
-    .setTitle("**__CUTIE__**")
-    .setColor(0xffd1dc)
-    .setDescription(`This bot is a homebrew bot and is being created to try and reduce the number of bots we have on the server. The name is subject to change.
+  if (args.length < 1) {
+    const embed = new MessageEmbed()
+      .setTitle("**__CUTIE__**")
+      .setColor(0xffd1dc)
+      .setDescription(`This bot is a homebrew bot and is being created to try and reduce the number of bots we have on the server. The name is subject to change.
 
-  Its current prefix is "qt"
+    Its current prefix is "qt"
 
-  ***Current Command List:***
+    ***Current Command List:***
 
-  __*Support*__
-  **spoilers** - tutorial on how to use spoilers
-  **spoilers-guide** - info on when and where to use spoilers
-  **hotlines** - sends a message with links containing helpline/hotlines from various countries
-  **breathe** - sends 1 out of 3 gifs to breathe along to
-  **graphics** - sends a randomized graphic containing helpful self-care info
-  **coping** - sends a super short questionnaire to help you cope
-  **distractme** - sends a cute video to help take your mind off of things
+    __*Support*__
 
-  __*Miscellaneous:*__
-  **ping** - checks if the bot is working or not
-  **aww** - sends a random image/post from r/aww`);
-  message.channel.send(embed);
+    **spoilers** - tutorial on how to use spoilers
+    **spoilers-guide** - info on when and where to use spoilers
+    **hotlines** - sends a message with links containing helpline/hotlines from various countries
+    **breathe** - sends 1 out of 3 gifs to breathe along to
+    **graphics** - sends a randomized graphic containing helpful self-care info
+    **coping** - sends a super short questionnaire to help you cope
+    **distractme** - sends a cute video to help take your mind off of things
+
+    __*Miscellaneous:*__
+
+    **ping** - checks if the bot is working or not
+    **aww** - sends a random image/post from r/aww`);
+    message.channel.send(embed);
+  }
+  else if (args[0] === "misc") {
+    const embed = new MessageEmbed()
+      .setTitle("**__CUTIE__**")
+      .setColor(0xffd1dc)
+      .setDescription(`This bot is a homebrew bot and is being created to try and reduce the number of bots we have on the server. The name is subject to change.
+
+    Its current prefix is "qt"
+
+    __*Miscellaneous commands:*__
+
+    **ping** - checks if the bot is working or not
+    **aww** - sends a random image/post from r/aww`);
+    message.channel.send(embed);
+  }
+  else if (args[0] === "support") {
+    const embed = new MessageEmbed()
+      .setTitle("**__CUTIE__**")
+      .setColor(0xffd1dc)
+      .setDescription(`This bot is a homebrew bot and is being created to try and reduce the number of bots we have on the server. The name is subject to change.
+
+    Its current prefix is "qt"
+
+    __*Support commands*__
+    
+    **spoilers** - tutorial on how to use spoilers
+    **spoilers-guide** - info on when and where to use spoilers
+    **hotlines** - sends a message with links containing helpline/hotlines from various countries
+    **breathe** - sends 1 out of 3 gifs to breathe along to
+    **graphics** - sends a randomized graphic containing helpful self-care info
+    **coping** - sends a super short questionnaire to help you cope
+    **distractme** - sends a cute video to help take your mind off of things`);
+    message.channel.send(embed);
+  }
 }
