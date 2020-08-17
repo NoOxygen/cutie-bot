c97ab03b9cda1f0e38cb38a17b5b6af5b6d0044d
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-08-17 14:14:53 +0530

    Create a help command
    and some backend output so that host knows if bot went online successfully
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

---
+++ commands/help.js
@@ -0,0 +1,25 @@
+exports.run = (client, message, args) => {
+  const { Client, MessageEmbed } = require('discord.js');
+  const embed = new MessageEmbed()
+    .setTitle("**__CUTIE__**")
+    .setColor(0xffd1dc)
+    .setDescription(`This bot is a homebrew bot and is being created to try and reduce the number of bots we have on the server. The name is subject to change.
+
+  Its current prefix is "qt"
+
+  ***Current Command List:***
+
+  __*Support*__
+  **spoilers** - tutorial on how to use spoilers
+  **spoilers-guide** - info on when and where to use spoilers
+  **hotlines** - sends a message with links containing helpline/hotlines from various countries
+  **breathe** - sends 1 out of 3 gifs to breathe along to
+  **graphics** - sends a randomized graphic containing helpful self-care info
+  **coping** - sends a super short questionnaire to help you cope
+  **distractme** - sends a cute video to help take your mind off of things
+
+  __*Miscellaneous:*__
+  **ping** - checks if the bot is working or not
+  **aww** - sends a random image/post from r/aww`);
+  message.channel.send(embed);
+}
--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,7 @@
 module.exports = client => {
   // set activity status
   client.user.setActivity(`qt help`, {type: "LISTENING"});
+
+  // tell host that bot is online
+  console.log(`CUTIE v1.0.0 is now online`);
 };
