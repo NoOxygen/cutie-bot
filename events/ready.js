c45da7cb03fd7c7076af95e2380b585698e009bd
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-08-19 10:13:01 +0530

    Bump version
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = client => {
   // set activity status
-  client.user.setActivity(`qt help | v1.1`, {type: "LISTENING"});
+  client.user.setActivity(`qt help | v1.2`, {type: "LISTENING"});
 
   // tell host that bot is online
-  console.log(`CUTIE v1.1.0 is now online`);
+  console.log(`CUTIE v1.2 is now online`);
 };
