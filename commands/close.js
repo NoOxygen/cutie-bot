e4138ff6e77c3adba2112a99521715e02ffe5916
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-08-30 14:15:25 +0530

    Patch ticket close permission override
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

--- commands/close.js
+++ commands/close.js
@@ -1,9 +1,21 @@
 exports.run = (client, message) => {
+  let staffRole = message.guild.roles.cache.find(role => role.name === "staff").id;
+  let aideRole = message.guild.roles.cache.find(role => role.name === "aide").id;
+  if (!aideRole) {};
+
   if(!message.channel.name.includes("ticket-")) return message.channel.send("You cannot use that here!")
   message.channel.overwritePermissions([
   {
     id: message.guild.id,
     deny: ["VIEW_CHANNEL", "SEND_MESSAGES"]
+  },
+  {
+    id: staffRole,
+    allow: ["VIEW_CHANNEL", "SEND_MESSAGES"]
+  },
+  {
+    id: aideRole,
+    allow: ["VIEW_CHANNEL", "SEND_MESSAGES"]
   }
 ]);
   message.channel.send("This channel has been archived for staff to check for any rule-breaking. Once it has been checked, the staff is expected to manually delete the channel")
