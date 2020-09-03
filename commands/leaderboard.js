45c883711cc61038f281b79c5caf07c5ad39c654
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-09-03 11:53:59 +0530

    Allow viewing full leaderboard
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,4 +1,4 @@
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
   const { Client, MessageEmbed } = require('discord.js');
   // Get a filtered list (for this guild only), and convert to an array while we're at it.
   const filtered = client.points.filter( p => p.guild === message.guild.id ).array();
@@ -6,8 +6,11 @@ exports.run = (client, message, args) => {
   // Sort it to get the top results... well... at the top. Y'know.
   const sorted = filtered.sort((a, b) => b.points - a.points);
 
+  var p = 0
+  var q = 10
+
   // Slice it, dice it, get the top 10 of it!
-  const top10 = sorted.splice(0, 10);
+  const top10 = sorted.splice(p, q);
 
   // Now shake it and show it! (as a nice embed, too!)
   const embed = new MessageEmbed()
@@ -18,5 +21,58 @@ exports.run = (client, message, args) => {
   for(const data of top10) {
     embed.addField(`${data.username}`, `${data.points} points (level ${data.level})`);
   }
-  return message.channel.send({embed});
+  try{
+    var leaderboard = await message.channel.send({embed});
+    await leaderboard.react("⬅️");
+    await leaderboard.react("➡️");
+  } catch (error) {
+    console.error(error);
+  }
+
+  const filter = (reaction, user) => user.id !== message.client.user.id;
+  var collector = leaderboard.createReactionCollector(filter, {
+    time: 60000
+  });
+
+  collector.on("collect", (reaction, user) => {
+    const member = message.guild.member(user);
+
+    switch (reaction.emoji.name) {
+      case "➡️":
+        reaction.users.remove(user).catch(console.error);
+        p = p + 10
+        q = q + 10
+        const nextEmbed = new MessageEmbed()
+          .setTitle("Leaderboard")
+          .setAuthor(client.user.username, client.user.avatarURL)
+          .setDescription("More places")
+          .setColor(0x00AE86);
+        for(const data of top10) {
+          embed.addField(`${data.username}`, `${data.points} points (level ${data.level})`);}
+        leaderboard.edit(nextEmbed);
+        break;
+
+      case "⬅️":
+        reaction.users.remove(user).catch(console.error);
+        p = p - 10
+        q = q - 10
+        if (p < 0) return
+        const prevEmbed = new MessageEmbed()
+          .setTitle("Leaderboard")
+          .setAuthor(client.user.username, client.user.avatarURL)
+          .setDescription("More places")
+          .setColor(0x00AE86);
+        for(const data of top10) {
+          embed.addField(`${data.username}`, `${data.points} points (level ${data.level})`);}
+        leaderboard.edit(prevEmbed);
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  collector.on("end", () => {
+    leaderboard.reactions.removeAll().catch(console.error);
+  });
 }
