742a0db63384dafa10a3b765953fcca37ff3f44c
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-09-04 10:46:29 +0530

    Attempt to fix leaderboard
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,16 +1,16 @@
 exports.run = async (client, message, args) => {
   const { Client, MessageEmbed } = require('discord.js');
   // Get a filtered list (for this guild only), and convert to an array while we're at it.
-  const filtered = client.points.filter( p => p.guild === message.guild.id ).array();
+  var filtered = client.points.filter( p => p.guild === message.guild.id ).array();
 
   // Sort it to get the top results... well... at the top. Y'know.
-  const sorted = filtered.sort((a, b) => b.points - a.points);
+  var sorted = filtered.sort((a, b) => b.points - a.points);
 
-  var p = 0
-  var q = 10
+  var i = 0
+  var j = 10
 
   // Slice it, dice it, get the top 10 of it!
-  const top10 = sorted.splice(p, q);
+  var top10 = sorted.splice(i, j);
 
   // Now shake it and show it! (as a nice embed, too!)
   const embed = new MessageEmbed()
@@ -34,41 +34,41 @@ exports.run = async (client, message, args) => {
     time: 60000
   });
 
-  collector.on("collect", (reaction, user) => {
+  collector.on("collect", async (reaction, user) => {
     const member = message.guild.member(user);
 
-    switch (reaction.emoji.name) {
-      case "➡️":
-        reaction.users.remove(user).catch(console.error);
-        p = p + 10
-        q = q + 10
-        const nextEmbed = new MessageEmbed()
-          .setTitle("Leaderboard")
-          .setAuthor(client.user.username, client.user.avatarURL)
-          .setDescription("More places")
-          .setColor(0xffd1dc);
-        for(const data of top10) {
-          embed.addField(`${data.username}`, `${data.points} points (level ${data.level})`);}
-        leaderboard.edit(nextEmbed);
-        break;
-
-      case "⬅️":
-        reaction.users.remove(user).catch(console.error);
-        p = p - 10
-        q = q - 10
-        if (p < 0) return
-        const prevEmbed = new MessageEmbed()
-          .setTitle("Leaderboard")
-          .setAuthor(client.user.username, client.user.avatarURL)
-          .setDescription("More places")
-          .setColor(0xffd1dc);
-        for(const data of top10) {
-          embed.addField(`${data.username}`, `${data.points} points (level ${data.level})`);}
-        leaderboard.edit(prevEmbed);
-        break;
-
-      default:
-        break;
+    if (reaction.emoji.name === "➡️") {
+      var newfiltered = client.points.filter( p => p.guild === message.guild.id ).array();
+      var newsorted = newfiltered.sort((a, b) => b.points - a.points);
+      reaction.users.remove(user).catch(console.error);
+      i = i + 10
+      j = j + 10
+      var topNext = newsorted.splice(i, j);
+      console.log(topNext);
+      const nextEmbed = new MessageEmbed()
+        .setTitle("Leaderboard")
+        .setAuthor(client.user.username, client.user.avatarURL)
+        .setDescription("More places")
+        .setColor(0xffd1dc);
+      for(const data of topNext) {
+        embed.addField(`${data.username}`, `${data.points} points (level ${data.level})`);}
+      await leaderboard.edit(nextEmbed);
+    } else if (reaction.emoji.name === "⬅️"){
+      var oldfiltered = client.points.filter( p => p.guild === message.guild.id ).array();
+      var oldsorted = oldfiltered.sort((a, b) => b.points - a.points);
+      reaction.users.remove(user).catch(console.error);
+      i = i - 10
+      j = j - 10
+      if (i < 0) return;
+      var topPrev = oldsorted.splice(i, j);
+      const prevEmbed = new MessageEmbed()
+        .setTitle("Leaderboard")
+        .setAuthor(client.user.username, client.user.avatarURL)
+        .setDescription("More places")
+        .setColor(0xffd1dc);
+      for(const data of topPrev) {
+        embed.addField(`${data.username}`, `${data.points} points (level ${data.level})`);}
+      await leaderboard.edit(prevEmbed);
     }
   });
 
