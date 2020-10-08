64018ec640b6e5103dcfa3cef9b6f391dcdf8fea
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-10-08 20:45:20 +0530

    Fix leave command derp
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

--- commands/leave.js
+++ commands/leave.js
@@ -1,5 +1,6 @@
 exports.run = (client, message) => {
   const queue = message.client.queue.get(message.guild.id);
+	if (!queue) return;
   queue.channel.leave();
   message.client.queue.delete(message.guild.id);
 }
