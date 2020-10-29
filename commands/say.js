fa00ca4de61f4295f4718989ff35dc5981b2b08a
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-10-29 10:07:45 +0530

    Patch mod command

--- commands/say.js
+++ commands/say.js
@@ -1,14 +1,11 @@
 exports.run = (client, message, [chnl, ...text]) => {
-	if(!message.member.hasPermission('ADMINISTRATOR'))
-    return;
-
   if(!message.member.hasPermission('MANAGE_MESSAGES'))
     return;
 
   var checkChannel = message.mentions.channels.first().id
 
   message.guild.channels.cache.get(checkChannel).startTyping();
-  var timeout = text.length * 150
+  var timeout = text.length * 250
 	setTimeout(() => {
 		client.channels.cache.get(checkChannel).send(`${text.join(" ")}`).then((message) => {
 			client.channels.cache.get(checkChannel).stopTyping();
