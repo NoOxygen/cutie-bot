99f81144828ff0085e751dfb0b161caf81a5651e
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-11-02 10:11:22 +0530

    Patch userinfo command

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -30,7 +30,7 @@ exports.run = async (client, message, args, level) => {
 		.addField("Status", `${status[member.user.presence.status]}`, true)
 		.addField("Playing", `${member.user.presence.activities.state ? `${member.user.presence.activities.state}` : "Not playing anything."}`, true)
 		.addField("Roles", `${member.roles.cache.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
-		.addField("Joined", `${moment().diff(moment.utc(member.joinedAt).format("MMMM Do YYYY"))} Days Ago (Created ${moment.utc(member.user.createdAt).format("MMMM Do YYYY")})`, true)
+		.addField("Joined", `${moment().diff(moment.utc(member.joinedAt), 'days')} Days Ago (Created ${moment.utc(member.user.createdAt).format("MMMM Do YYYY")})`, true)
 
 	message.channel.send({embed});
 }
