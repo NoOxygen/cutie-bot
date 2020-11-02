5caf0c951eaa2353fa7eecc104b65e1a0e1b15d1
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-11-02 10:30:03 +0530

    Use inline fields on serverinfo

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -8,12 +8,12 @@ exports.run = async (client, message) => {
             .setThumbnail(`${message.guild.icon != null ? message.guild.iconURL() : "https://discordapp.com/assets/dd4dbc0016779df1378e7812eabaa04d.png"}`)
 						.setAuthor(`${message.guild} (${message.guild.id})`)
             .addField("Owner", `${message.guild.owner}`)
-            .addField("Member Count", `${message.guild.memberCount}`)
-            .addField("Emoji Count", `${message.guild.emojis.cache.size}`)
-            .addField("Roles Count", `${message.guild.roles.cache.size}`)
-						.addField("Text Channels", `${message.guild.channels.cache.filter((c) => c.type === "text").size}`)
-						.addField("Voice Channels", `${message.guild.channels.cache.filter((c) => c.type === "voice").size}`)
-						.addField("Created On", `${moment.utc(message.guild.createdAt).format("MMMM Do YYYY")}`)
+            .addField("Member Count", `${message.guild.memberCount}`, true)
+            .addField("Emoji Count", `${message.guild.emojis.cache.size}`, true)
+            .addField("Roles Count", `${message.guild.roles.cache.size}`, true)
+						.addField("Text Channels", `${message.guild.channels.cache.filter((c) => c.type === "text").size}`, true)
+						.addField("Voice Channels", `${message.guild.channels.cache.filter((c) => c.type === "voice").size}`, true)
+						.addField("Created On", `${moment.utc(message.guild.createdAt).format("MMMM Do YYYY")}`, true)
 
 
         message.channel.send(embed)
