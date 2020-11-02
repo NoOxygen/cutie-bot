19a6056900642e7a4e53321fddacc9f2182d146d
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-11-02 10:05:39 +0530

    Add a serverinfo command

---
+++ commands/serverinfo.js
@@ -0,0 +1,20 @@
+exports.run = async (client, message) => {
+	const Discord = require("discord.js");
+	const moment = require("moment-timezone");
+	require("moment-duration-format");
+
+	let embed = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setThumbnail(`${message.guild.icon != null ? message.guild.iconURL() : "https://discordapp.com/assets/dd4dbc0016779df1378e7812eabaa04d.png"}`)
+						.setAuthor(`${message.guild} (${message.guild.id})`)
+            .addField("Owner", `${message.guild.owner}`)
+            .addField("Member Count", `${message.guild.memberCount}`)
+            .addField("Emoji Count", `${message.guild.emojis.cache.size}`)
+            .addField("Roles Count", `${message.guild.roles.cache.size}`)
+						.addField("Text Channels", `${message.guild.channels.cache.filter((c) => c.type === "text").size}`)
+						.addField("Voice Channels", `${message.guild.channels.cache.filter((c) => c.type === "voice").size}`)
+						.addField("Created On", `${moment.utc(message.guild.createdAt).format("MMMM Do YYYY")}`)
+
+
+        message.channel.send(embed)
+}
