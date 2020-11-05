0e5847f2e18536a9938d2fb635a84a25d004f6f9
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-11-05 12:41:24 +0530

    Bump version

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = client => {
   // set activity status
-  client.user.setActivity(`qt help | v3.4`, {type: "LISTENING"});
+  client.user.setActivity(`qt help | v3.4.u`, {type: "LISTENING"});
 
   // tell host that bot is online
-  console.log(`CUTIE v3.4 is now online`);
+  console.log(`CUTIE v3.4.u is now online`);
 };
