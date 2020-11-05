a95aadf0dd280aae457acbbdadfa59170cb58c60
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-11-05 14:51:38 +0530

    Add more ways to update

--- commands/update.js
+++ commands/update.js
@@ -3,7 +3,14 @@ exports.run = async(client, message, args, level) => { // eslint-disable-line no
     return;
 	const sh = require('shelljs');
 
-  var output = sh.exec('git pull').stdout;
-	message.channel.send("```" + output + "```")
-	sh.exec('npm install')
+  if(args[0] === 'force'){
+		sh.exec('git reset --hard HEAD~10')
+		var output = sh.exec('git pull').stdout;
+		message.channel.send("```" + output + "```")
+	} else if (args[0] === 'npm') {
+		sh.exec('npm install')
+	} else {
+		var output = sh.exec('git pull').stdout;
+		message.channel.send("```" + output + "```")
+	}
 };
