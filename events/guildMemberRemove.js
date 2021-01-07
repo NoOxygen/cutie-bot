943ab99e0f3449f1738ebfaef70777c3a5a466e3
Siddharth Pranjale <sidpranjale127@protonmail.com>
2021-01-07 11:17:23 +0530

    Special request for Org.
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,15 +1,20 @@
 module.exports = (client, member) => {
-	const { MessageEmbed } = require("discord.js");
+    const { MessageEmbed } = require("discord.js");
 
-	const key = `${member.guild.id}`
+    const key = `${member.guild.id}`
 
-	const embed = new MessageEmbed()
-		.setColor(0xffd1dc)
-		.setDescription(`${member.user} left the server`)
-    .setTimestamp();
+    const embed = new MessageEmbed()
+        .setColor(0xffd1dc)
+        .setDescription(`${member.user} left the server`)
+        .setTimestamp();
 
-	if (!client.logger.has(key)) return;
-	if (client.logger.get(key, "logChnl") === null) return;
-	const sendChnl = client.logger.get(key, "logChnl")
-	client.channels.cache.get(sendChnl).send(embed).catch(console.error);
+    if (!client.logger.has(key)) return;
+    if (key == '720532324802977793') {
+        const sendChnl = message.guild.channels.cache.find(channel => channel.name === "staff")
+        client.channels.cache.get(sendChnl).send(embed).catch(console.error)
+        return;
+    }
+    if (client.logger.get(key, "logChnl") === null) return;
+    const sendChnl = client.logger.get(key, "logChnl")
+    client.channels.cache.get(sendChnl).send(embed).catch(console.error);
 }
