d1b47b92775b5cd58917e1fcebbfcd878264c4b9
Siddharth Pranjale <sidpranjale127@protonmail.com>
2021-01-07 12:45:25 +0530

    Make utility lighter
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

--- commands/update.js
+++ commands/update.js
@@ -1,16 +1,16 @@
 exports.run = async(client, message, args, level) => { // eslint-disable-line no-unused-vars
-	if(!message.member.hasPermission('ADMINISTRATOR'))
-    return;
-	const sh = require('shelljs');
+    if (!message.member.hasPermission('ADMINISTRATOR'))
+        return;
+    const sh = require('shelljs');
 
-  if(args[0] === 'force'){
-		sh.exec('git reset --hard HEAD~10')
-		var output = sh.exec('git pull').stdout;
-		message.channel.send("```" + output + "```")
-	} else if (args[0] === 'npm') {
-		sh.exec('npm install')
-	} else {
-		var output = sh.exec('git pull').stdout;
-		message.channel.send("```" + output + "```")
-	}
+    if (args[0] === 'force') {
+        sh.exec(`git reset --hard HEAD~${parseInt(args[1])}`)
+        var output = sh.exec('git pull').stdout;
+        message.channel.send("```" + output + "```")
+    } else if (args[0] === 'npm') {
+        sh.exec('npm install')
+    } else {
+        var output = sh.exec('git pull').stdout;
+        message.channel.send("```" + output + "```")
+    }
 };
