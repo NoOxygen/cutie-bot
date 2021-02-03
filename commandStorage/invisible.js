422efff0275236601322c9df73dcff210578a802
Siddharth Pranjale <sidpranjale127@protonmail.com>
2021-02-03 17:58:08 +0530

    Utils update
    Signed-off-by: Siddharth Pranjale <sidpranjale127@protonmail.com>

---
+++ commandStorage/invisible.js
@@ -0,0 +1,13 @@
+module.exports = client => {
+    // set activity status
+    client.user.setPresence({
+        status: 'invisible',
+        activity: {
+            name: "qt help | v3.5",
+            type: "LISTENING"
+        }
+    });
+
+    // tell host that bot is online
+    console.log(`CUTIE v3.5 is now online`);
+};
--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,13 @@
 module.exports = client => {
-  // set activity status
-  client.user.setActivity(`qt help | v3.5`, {type: "LISTENING"});
+    // set activity status
+    client.user.setPresence({
+        status: 'online',
+        activity: {
+            name: "qt help | v3.5",
+            type: "LISTENING"
+        }
+    });
 
-  // tell host that bot is online
-  console.log(`CUTIE v3.5 is now online`);
+    // tell host that bot is online
+    console.log(`CUTIE v3.5 is now online`);
 };
